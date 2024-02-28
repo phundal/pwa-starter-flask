ee2bfbd428ba6d374abeae61dae939f88b4af3a2
Patrick Hundal <phundal@gmail.com>
2024-02-27 16:56:27 -0800

    initial upload

---
+++ templates/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_from_directory, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_from_directory('static/', 'manifest.json')
+
+@app.route('/sw.js')
+def serve_sw():
+    return send_from_directory('static/', 'sw.js')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='54321')
